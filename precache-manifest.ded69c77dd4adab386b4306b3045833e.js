c21224bcad7b85a9846e879443c4cdccf0cbe866
forgreatness <danh1nguyen23@live.com>
2020-08-19 14:55:48 -0700

    Updates

---
+++ precache-manifest.ded69c77dd4adab386b4306b3045833e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3832f6b2bbfb22f7173c79be7e352c8a",
+    "url": "/emotion_styled_web_app/index.html"
+  },
+  {
+    "revision": "63b6a122be069cb503aa",
+    "url": "/emotion_styled_web_app/static/js/2.3b2fff72.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/emotion_styled_web_app/static/js/2.3b2fff72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9054b865e12930e40b87",
+    "url": "/emotion_styled_web_app/static/js/main.d3d16a77.chunk.js"
+  },
+  {
+    "revision": "792dbfc72f75fd5525d2",
+    "url": "/emotion_styled_web_app/static/js/runtime-main.6435cfb1.js"
+  }
+]);
